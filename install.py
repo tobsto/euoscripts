3f514199713390759b0460e71026fdcfda7e7f0f
Tobias Stollenwerk <stollenwerk@th.physik.uni-bonn.de>
2012-11-29 22:52:49 +0100

    Updated bin path for heisenberg

--- install.py
+++ install.py
@@ -2,9 +2,9 @@
 
 import subprocess
 
-programms=['euo.py', 'get_total_magnetisation.py','job.py', 'pararun.py','reflect.py','run.py','update.py', 'fetchdeb.py']
+programms=['euo.py', 'get_total_magnetisation.py','job.py', 'pararun.py','reflect.py','run.py','update.py', 'fetchdeb.py', 'printisodeltas.py']
 
-destinations=['/home/stollenw/sonstiges/programms/bin','/mnt/stollenw/sonstiges/local/python', '/mnt/debcluster/programms/bin']
+destinations=['/home/stollenw/Sonstiges/Programme/bin','/mnt/stollenw/sonstiges/local/python', '/mnt/debcluster/programms/bin']
 for d in destinations:
 	for p in programms:
 		cmd="cp %s %s" % (p,d)
