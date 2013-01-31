1e2f2752209d2dd205770e6ec9f0410030a59484
Tobias Stollenwerk <stollenwerk@th.physik.uni-bonn.de>
2013-01-31 12:54:34 +0100

    Restructured conductivity calculation
    In the previous version, the download of data was very unefficient.

--- getcond.py
+++ getcond.py
@@ -18,8 +18,7 @@ def get_worker():
 	print "Error: get_worker: %s is an unknown working host. Break." % host
 	exit(1)
 	
-def getcond(inputFolder, np=1, isodeltadb=False, outputFolder=None):
-	mpicmd=get_worker().mpicmd
+def getcond(inputFolder, mpicmd, np=1, isodeltadb=False, outputFolder=None):
 	runcmd=mpicmd + " -np %i" % np
 	runcmd=runcmd + " getcond.out -i %s" % inputFolder
 	if outputFolder!=None:
@@ -33,128 +32,119 @@ def getcond(inputFolder, np=1, isodeltadb=False, outputFolder=None):
 	#print runcmd
 	subprocess.call(runcmd, shell=True)
 
-def remote_file_exists(host, path):
-	cmd = "ssh %s '[[ -f %s ]]'" % (host, path)
-	proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-	proc.communicate()
-	if proc.returncode==0:
-		return True
+def getcondremote(host, db, filenames, inputFolder, mpicmd, np=1, isodeltadb=False, outputFolder=None):
+	if host=='heisenberg':
+		getcond(inputFolder, mpicmd, np, isodeltadb, outputFolder)
 	else:
-		return False	
+		print "download ..."
+		cmd = "rsync -avz stollenw@heisenberg.physik.uni-bonn.de:%s/results temp_cond/" % inputFolder
+		cmd = cmd+ "; rsync -avz stollenw@heisenberg.physik.uni-bonn.de:%s/parameter.cfg temp_cond/" % inputFolder
+		#print cmd
+		proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+		proc.communicate()
+
+		print "calculate ..."
+		getcond("temp_cond/", mpicmd, np, isodeltadb, outputFolder)
+
+		print "upload ..."
+		cmd='scp '
+		for filename in filenames:
+			cmd=cmd+"temp_cond/results/%s " % filename
+		cmd=cmd+"stollenw@heisenberg.physik.uni-bonn.de:%s/results/" % inputFolder
+
+		#print cmd
+		proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+		proc.communicate()
+		print "done"
+				
+
+def check_file_exists(host, path):
+	if host=='heisenberg':
+		return os.path.exists(path)
+	else:
+		cmd = "ssh %s '[[ -f %s ]]'" % ('stollenw@heisenberg.physik.uni-bonn.de', path)
+		proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+		proc.communicate()
+		if proc.returncode==0:
+			return True
+		else:
+			return False	
+
 	
 def main():
 	parser = argparse.ArgumentParser(description='Calculate conductivity out of euo program results')
-	parser.add_argument('-i', '--input', help='Input folder containing results. If not given, calculate the conductivity for all results from all databases')
-	parser.add_argument('--no_isodelta_db', action='store_false', help='No fetching of more accurate isodeltas in the case of heterostructures')
+	parser.add_argument('-d', '--database', help='specify database')
+	parser.add_argument('-s', '--dataset', nargs='+', help='specify dataset without temperature')
 	parser.add_argument('-n', '--np', default=1, help='Number of processes for parallel use', type=int)
-	parser.add_argument('-o', '--output', default=None, help='Output Folder')
-	parser.add_argument('--overwrite', action="store_true", help='recalculate conductivity in all results from all databases (hetero, isolated, bulk)')
-	parser.add_argument('-d', '--db', default='all', help='specify database')
+	parser.add_argument('-i', '--input', help='Input folder containing result of a single run.')
+	parser.add_argument('-o', '--output', default=None, help='Output Folder for the single run results (optional, default=input)')
+	parser.add_argument('--no_isodelta_db', action='store_false', help='No fetching of more accurate isodeltas in the case of heterostructures')
+	parser.add_argument('--overwrite', action="store_true", help='recalculate conductivity, even if it is present in the database')
+	#parser.add_argument('--force_download', action="store_true", help='Download *all* results from database, even if they exist in the current folder')
 	args = parser.parse_args()
 	
-	flag_bulk=False
-	flag_iso=False
-	flag_hetero=False
-	if args.db=='all':
-		flag_bulk=True
-		flag_iso=True
-		flag_hetero=True
-	elif args.db=='bulk':
-		flag_bulk=True
-	elif args.db=='isolated':
-		flag_iso=True
-	elif args.db=='hetero':
-		flag_hetero=True
+	if not args.database in ('bulk', 'isolated', 'hetero'):
+		print "Database must be 'bulk', 'isolated' or 'hetero'"
+		exit(1)
 		
+	# remote or heisenberg
+	host=database.get_host()
+	# get mpi run command depening on host
+	mpicmd=get_worker().mpicmd
+
 	# add current working directory to system path
 	sys.path.append(os.getcwd())
 
 	if not args.input==None:
 		getcond(args.input, np=args.np, isodeltadb=args.no_isodelta_db, outputFolder=args.output)
 	else:
-		if flag_bulk:
+		if args.database=='bulk':
 			# bulk database
 			print "Bulk database"
+			filenames=("cond.dat", "resist.dat")
 			bdb=database.bulk_database()	
 			bdb.download()
-			temp_output="temp_cond/bulk/"
 			for (material, ni, T, mag, path) in bdb.data:
-				result_folder = bdb.get_output(material, ni) + bdb.get_temp_output(T)
-				temp_result_folder = temp_output + result_folder 
+				result_folder = "/home/stollenw/projects/euo/results/bulk/" + bdb.get_output(material, ni) + bdb.get_temp_output(T)
 				# check if conductivity calculation was already performed
 				print "check %s, ni=%f, T=%f ..." % (material, ni, T)
-				exists=remote_file_exists('stollenw@heisenberg.physik.uni-bonn.de', '/home/stollenw/projects/euo/results/bulk/%s/results/cond.dat' % result_folder)
+				exists=check_file_exists(host, '%s/results/cond.dat' % result_folder)
 				# calculate conductivity if necessary or forced
 				if not exists or args.overwrite:
-					bdb.download_results(material, ni, T, temp_output)
 					print "calculate %s, ni=%f, T=%f ..." % (material, ni, T)
-					getcond(temp_result_folder, np=args.np, isodeltadb=False)
-	
-					# copy conductivity results to result-database
-					result_names=("cond.dat", "resist.dat")
-					cmd='scp '
-					for filename in result_names:
-						cmd=cmd+"%s/results/%s " % (temp_result_folder, filename)
-					cmd=cmd+"stollenw@heisenberg.physik.uni-bonn.de:/home/stollenw/projects/euo/results/bulk/%s/results/" % result_folder
-					#print cmd
-					proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-					proc.wait()
-		if flag_iso:
+					getcondremote(host, bdb, filenames, result_folder, mpicmd, np=args.np, isodeltadb=False)
+
+		if args.database=='isolated':
 			# isolated database
 			print "Isolated database"
+			filenames=("cond.dat", "resist.dat", "cond_perp.dat", "resist_perp.dat", "cond_perp_matrix.dat", "resist_perp_matrix.dat")
 			idb=database.isolated_database()	
 			idb.download()
-			temp_output="temp_cond/isolated/"
 			for (material, N, ni, T, isodelta, path) in idb.data:
-				result_folder = idb.get_output(material, N, ni) + idb.get_temp_output(T)
-				temp_result_folder = temp_output + result_folder 
+				result_folder = "/home/stollenw/projects/euo/results/isolated/" + idb.get_output(material, N, ni) + idb.get_temp_output(T)
 				# check if conductivity calculation was already performed
 				print "check %s, N=%i, ni=%f, T=%f ..." % (material, N, ni, T)
-				exists=remote_file_exists('stollenw@heisenberg.physik.uni-bonn.de', '/home/stollenw/projects/euo/results/isolated/%s/results/cond_perp_matrix.dat' % result_folder)
+				exists=check_file_exists(host, '%s/results/cond_perp_matrix.dat' % result_folder)
 				# calculate conductivity if necessary or forced
 				if not exists or args.overwrite:
-					idb.download_results(material, N, ni, T, temp_output)
 					print "calculate %s, N=%i, ni=%f, T=%f ..." % (material, N, ni, T)
-					getcond(temp_result_folder, np=args.np, isodeltadb=False)
+					getcondremote(host, idb, filenames, result_folder, mpicmd, np=args.np, isodeltadb=False)
 	
-					# copy conductivity results to result-database
-					result_names=("cond.dat", "resist.dat", "cond_perp.dat", "resist_perp.dat", "cond_perp_matrix.dat", "resist_perp_matrix.dat")
-					cmd='scp '
-					for filename in result_names:
-						cmd=cmd+"%s/results/%s " % (temp_result_folder, filename)
-					cmd=cmd+"stollenw@heisenberg.physik.uni-bonn.de:/home/stollenw/projects/euo/results/isolated/%s/results/" % result_folder
-					#print cmd
-					proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-					proc.wait()
-	
-		if flag_hetero:
+		if args.database=='hetero':
 			# heterostructure database
 			print "Heterostructure database"
+			filenames=("cond.dat", "resist.dat", "cond_perp.dat", "resist_perp.dat", "cond_perp_matrix.dat", "resist_perp_matrix.dat")
 			hdb=database.heterostructure_database()	
 			hdb.download()
-			temp_output="temp_cond/heterostructure/"
 			for (material, N, M, ni, ncr, dW, T, avmag, path) in hdb.data:
-				result_folder = hdb.get_output(material, N, M, ni, ncr, dW) + hdb.get_temp_output(T)
-				temp_result_folder = temp_output + result_folder 
+				result_folder = "/home/stollenw/projects/euo/results/heterostructure/" + hdb.get_output(material, N, M, ni, ncr, dW) + hdb.get_temp_output(T)
 				# check if conductivity calculation was already performed
-				print "check %s, N=%i, M=%i, ni=%f, ncr=%f, T=%f ..." % (material, N, M, ni, ncr, T)
-				exists=remote_file_exists('stollenw@heisenberg.physik.uni-bonn.de', '/home/stollenw/projects/euo/results/heterostructure/%s/results/cond_perp_matrix.dat' % result_folder)
+				print "check %s, N=%i, M=%i, ni=%f, ncr=%f, dW=%f, T=%f ..." % (material, N, M, ni, ncr, dW, T)
+				exists=check_file_exists(host, '%s/results/cond_perp_matrix.dat' % result_folder)
 				# calculate conductivity if necessary or forced
 				if not exists or args.overwrite:
-					hdb.download_results(material, N, M, ni, ncr, dW, T, temp_output)
-					print "calculate %s, N=%i, M=%i, ni=%f, ncr=%f, T=%f ..." % (material, N, M, ni, ncr, T)
-					getcond(temp_result_folder, np=args.np, isodeltadb=args.no_isodelta_db)
-	
-					# copy conductivity results to result-database
-					result_names=("cond.dat", "resist.dat", "cond_perp.dat", "resist_perp.dat", "cond_perp_matrix.dat", "resist_perp_matrix.dat")
-					cmd='scp '
-					for filename in result_names:
-						cmd=cmd+"%s/results/%s " % (temp_result_folder, filename)
-					cmd=cmd+"stollenw@heisenberg.physik.uni-bonn.de:/home/stollenw/projects/euo/results/heterostructure/%s/results/" % result_folder
-					#print cmd
-					proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-					proc.wait()
-	
+					print "calculate %s, N=%i, M=%i, ni=%f, ncr=%f, dW=%f, T=%f ..." % (material, N, M, ni, ncr, dW, T)
+					getcondremote(host, hdb, filenames, result_folder, mpicmd, np=args.np, isodeltadb=args.no_isodelta_db)
 	
 if __name__=="__main__":
 	main()
